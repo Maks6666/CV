7e182be76fc4e8e3f5c513cbd97ddd282a5bd781
Maks Kucher <118250600+Maks6666@users.noreply.github.com>
2024-04-15 15:26:52 +0300

    Update group_detector.py
    Program will ignore single objects

--- group_detector.py
+++ group_detector.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 
-cap = cv2.VideoCapture("path/to/video")
+cap = cv2.VideoCapture("walking.mp4")
 
 ret, frame = cap.read()
 object_count = 0
@@ -35,10 +35,11 @@ while ret:
         x_center = int((x_min + x_max) / 2)
         y_center = int((y_min + y_max) / 2)
 
-        cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 0, 255), 2)
-        cv2.circle(frame, (x_center, y_center + 80), 3, (0, 0, 255), thickness=cv2.FILLED)
-        cv2.putText(frame, f"{names[int(class_id)].upper()}", (int(x_min), int(y_min)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
+        if len(person_coordinates) > 3:
+            cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 0, 255), 2)
+            cv2.circle(frame, (x_center, y_center + 100), 3, (0, 0, 255), thickness=cv2.FILLED)
+            cv2.putText(frame, "Detected group", (int(x_min), int(y_min)-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
 
     cv2.imshow("Video", frame)
 
